821671368bf163772aafcbc504cd0effdc856378
Raniya Poonthala <141708870+raniyaptla@users.noreply.github.com>
2025-08-07 07:17:03 +0530

    Update App.test.jsx

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,17 +1,15 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
-describe('App unit tests', () => {
-  test('renders heading and button', () => {
-    render(<App />)
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Training')
-    expect(screen.getByRole('button')).toHaveTextContent('count is 0')
-  })
+test('renders heading and button', () => {
+  render(<App />)
+  expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Training')
+  expect(screen.getByRole('button')).toHaveTextContent('count is 0')
+})
 
-  test('increments count when button is clicked', () => {
-    render(<App />)
-    const button = screen.getByRole('button')
-    fireEvent.click(button)
-    expect(button).toHaveTextContent('count is 1')
-  })
+test('increments count when button is clicked', () => {
+  render(<App />)
+  const button = screen.getByRole('button')
+  fireEvent.click(button)
+  expect(button).toHaveTextContent('count is 1')
 })
